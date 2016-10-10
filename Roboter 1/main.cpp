66ceae63a907eefbd91ca369c99228733215fda1
Christoph Barosch <barosch.christoph@hotmail.com>
2016-10-10 13:35:28 +0200

    10.10.2016

--- Roboter 1/main.cpp
+++ Roboter 1/main.cpp
@@ -3,5 +3,11 @@
 using namespace std;
 
 int main() {
-	cout << "TEST" << endl;
+	for (;;)
+	{	
+		motor(0, -50); // hinten links - langsam
+		motor(1, 100); // vorne links - geht ned
+		motor(2, 20); // hinten rechts
+		motor(3, 20); // vorne rechts
+	}
 }
