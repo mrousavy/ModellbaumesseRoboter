7d86c953aa78581374abd42e11926769e8285c20
Christoph Barosch <barosch.christoph@hotmail.com>
2016-10-20 16:23:53 +0200

    Chrisis letzte Version

--- main.c
+++ main.c
@@ -1,28 +1,55 @@
 void afterSpeedChange(int lastCase, int speed) {
-	switch (lastCase) {
+	int switch;
+	if (lastCase != -1) {
+		switch = lastCase;
+	} else {
+		switch = remote_read();
+	}
+	switch (switch) {
+		case -1:
+			return 0;
+			break;
+		case 0:
+			motor(0, 0);
+			motor(2, 0);
+			break;
 		case 1:
 			motor(0, speed);
-			motor(1, speed);
 			motor(2, -speed);
-			motor(3, -speed);
+			if (lastCase != -1) {
+				lastCase = -1;
+			}
 			break;
 		case 2:
 			motor(0, -speed);
-			motor(1, -speed);
 			motor(2, speed);
-			motor(3, speed);
+			lastCase = -1;
 			break;
 		case 3:
 			motor(0, -speed);
-			motor(1, -speed);
 			motor(2, -speed);
-			motor(3, -speed);
+			lastCase = -1;
 			break;
 		case 4:
 			motor(0, speed);
-			motor(1, speed);
 			motor(2, speed);
-			motor(3, speed);
+			lastCase = -1;
+			break;
+		case 5:
+			if (speed > 90) {
+				printf("Maximum Speed reached");
+			} else {
+				speed += 10;
+				afterSpeedChange(lastCase, speed);
+			}
+			break;
+		case 6:
+			if (speed < 10) {
+				printf("Minimum Speed reached");
+			} else {
+				speed -= 10;
+				afterSpeedChange(lastCase, speed);
+			}
 			break;
 	}
 }
@@ -32,62 +59,9 @@ int main() {
 	printf("Server setted up!");
 	int speed = 20;
 	int lastCase;
+	enable_servo(0);
 	while (1) {
-		switch (remote_read()) {
-			case -1:
-				return 0;
-				break;
-			case 0:
-				motor(0, 0);
-				motor(1, 0);
-				motor(2, 0);
-				motor(3, 0);
-				break;
-			case 1:
-				motor(0, speed);
-				motor(1, speed);
-				motor(2, -speed);
-				motor(3, -speed);
-				lastCase = 1;
-				break;
-			case 2:
-				motor(0, -speed);
-				motor(1, -speed);
-				motor(2, speed);
-				motor(3, speed);
-				lastCase = 2;
-				break;
-			case 3:
-				motor(0, -speed);
-				motor(1, -speed);
-				motor(2, -speed);
-				motor(3, -speed);
-				lastCase = 3;
-				break;
-			case 4:
-				motor(0, speed);
-				motor(1, speed);
-				motor(2, speed);
-				motor(3, speed);
-				lastCase = 4;
-				break;
-			case 5:
-				if (speed > 90) {
-					printf("Maximum Speed reached");
-				} else {
-					speed += 10;
-					afterSpeedChange(lastCase, speed);
-				}
-				break;
-			case 6:
-				if (speed < 10) {
-					printf("Minimum Speed reached");
-				} else {
-					speed -= 10;
-					afterSpeedChange(lastCase, speed);
-				}
-				break;
-		}
+		move(lastCase, speed);
 	}
 	return 0;
 }
